0ac57dfe14c5ec46f2996f76238efca022bfa4a4
Veerman14 <hasibuanfirman1404@gmail.com>
2025-06-30 18:15:50 +0700

    fix error

--- BACKEND/schemas/tweet.py
+++ BACKEND/schemas/tweet.py
@@ -13,7 +13,7 @@ class TweetResponse(TweetBase):
     is_depression: bool
 
     class Config:
-        orm_mode = True
+        from_attributes = True
 
 class TweetHistoryResponse(BaseModel):
     history: List[TweetResponse]
