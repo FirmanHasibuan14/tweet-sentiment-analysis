86794b7889e8adb712b441427a0308e27b88bd43
Veerman14 <hasibuanfirman1404@gmail.com>
2025-06-30 17:04:16 +0700

    add tweet schema

---
+++ BACKEND/schemas/tweet.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel
+from typing import List
+
+class TweetBase(BaseModel):
+    text: str
+
+class TweetCreate(TweetBase):
+    pass
+
+class TweetResponse(TweetBase):
+    id: int
+    sentiment_score: float
+    is_depression: bool
+
+    class Config:
+        orm_mode = True
+
+class TweetHistoryResponse(BaseModel):
+    history: List[TweetResponse]
